1acd04080ae596439def94106060e6fd0f7e2c06
Ulises Giacoman <ugiacoman@email.wm.edu>
2016-02-20 19:17:37 -0500

    added form to basic html

--- src/despabilado/views.py
+++ src/despabilado/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+    title = "Welcome"
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "form": form
+
+    }
+
+    # On form submit and valid
+    if form.is_valid():
+        # form.save()
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")
+
+        
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+
+        # if not instance.full_name:
+        #     instance.full_name = "Bruh"
+        instance.save()
+
+        context = {
+            "title": "Thank you"
+        }
+
+
+    return render(request, "home.html", context)
